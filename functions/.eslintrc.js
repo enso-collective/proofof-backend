914bd41e87349573b45c5eead0b7280688aaa127
Drew Cook <drew@dco.dev>
2023-10-18 07:05:58 -0600

    chore: setup emulators

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		sourceType: 'module',
-		project: 'tsconfig.json',
 		tsconfigRootDir: __dirname,
 	},
 	plugins: ['prettier', 'import', 'simple-import-sort', '@typescript-eslint'],
