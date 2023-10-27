b9a8f65447daf94304203e0fdbcbb9fd0a00c97f
Drew Cook <drew@dco.dev>
2023-10-26 21:04:46 -0600

    feat: add in retreive and verify credential endpoints

--- functions/src/routes/disco.ts
+++ functions/src/routes/disco.ts
@@ -19,23 +19,23 @@ export const issueDiscoCredential = functions.https.onRequest(async (req, res) =
 			email: supportedParams.email,
 		})
 
-		// Look up the user by email
-		const userSnapshot = await admin
-			.firestore()
-			.collection(COLLECTIONS.USERS)
-			.where('email', '==', supportedParams.email)
-			.get()
-		if (userSnapshot.empty)
-			throw new Error(`User with email '${supportedParams.email}' could not be found`)
-		const userRef = userSnapshot.docs[0].ref
-
 		// 2. Catch validation errors if any
 		if (validateDIDResult.error) {
 			res.status(400).json({ error: validateDIDResult.error })
 		} else if (validateEmailResult.error) {
 			res.status(400).json({ error: validateEmailResult.error })
 		} else {
-			// 3. Make a request to Disco API to issue a credential to the recipeint from the Enso Disco Org
+			// 3. Look up the user by email, fail if doesn't exist
+			const userSnapshot = await admin
+				.firestore()
+				.collection(COLLECTIONS.USERS)
+				.where('email', '==', supportedParams.email)
+				.get()
+			if (userSnapshot.empty)
+				throw new Error(`User with email '${supportedParams.email}' could not be found`)
+			const userRef = userSnapshot.docs[0].ref
+
+			// 4. Make a request to Disco API to issue a credential to the recipeint from the Enso Disco Org
 			// Construct headers
 			const headers = new Headers()
 			headers.append('Content-Type', 'application/json')
@@ -69,11 +69,139 @@ export const issueDiscoCredential = functions.https.onRequest(async (req, res) =
 			// Get Result
 			const result = JSON.parse(await response.text())
 
-			// 4.Store the entire result in the user's discoCredentials subcollection (could slim down what is important to persist in future iterations)
+			// 5.Store the entire result in the user's discoCredentials subcollection (could slim down what is important to persist in future iterations)
 			await userRef.collection(SUBCOLLECTIONS.DISCO_CREDENTIALS).add(result)
 
-			// 5. Return the VC's ID back to the client
+			// 6. Return the VC's ID back to the client
 			res.status(201).json({ data: result.vc.id })
+		}
+	} catch (error: any) {
+		res.status(500).json({ error: error.message })
+	}
+})
+
+// Kind of a redundant way to retreive the data via the API since we are choosing to store the entire object anyway, but if we choose to store only VC IDs, then we'll need to call the Disco API to retrieve the entire object back.
+export const retrieveDiscoCredential = functions.https.onRequest(async (req, res) => {
+	try {
+		// 1. Validate required inputs
+		const supportedParams = {
+			email: req.body.email,
+			recipientDID: req.body.recipientDID,
+			vcId: req.body.vcId,
+		}
+		const validateDIDResult = await validateAndConvert(DiscoRecipientDto, {
+			did: supportedParams.recipientDID,
+		})
+		const validateEmailResult = await validateAndConvert(UserEmailDto, {
+			email: supportedParams.email,
+		})
+
+		// 2. Catch validation errors if any
+		if (validateDIDResult.error) {
+			res.status(400).json({ error: validateDIDResult.error })
+		} else if (validateEmailResult.error) {
+			res.status(400).json({ error: validateEmailResult.error })
+		} else {
+			// 3a. Look up the user by email, fail if doesn't exist
+			const userSnapshot = await admin
+				.firestore()
+				.collection(COLLECTIONS.USERS)
+				.where('email', '==', supportedParams.email)
+				.get()
+			if (userSnapshot.empty)
+				throw new Error(`User with email '${supportedParams.email}' could not be found`)
+			const userRef = userSnapshot.docs[0].ref
+
+			// 3b. Look up the credential in the db, fail if doesn't exist
+			const userVcSnapshot = await userRef
+				.collection(SUBCOLLECTIONS.DISCO_CREDENTIALS)
+				.where('vc.id', '==', supportedParams.vcId)
+				.get()
+			if (userVcSnapshot.empty)
+				throw new Error(`VC with id ${supportedParams.vcId} could not be found`)
+
+			// 4. Make a request to Disco API to verify the credential is valid
+			const headers = new Headers()
+			headers.append('Content-Type', 'application/json')
+			headers.append('Accept', '*/*')
+			headers.append('Authorization', `Bearer ${process.env.DISCO_ORG_API_KEY}`)
+			const response = await fetch(
+				`https://api.disco.xyz/v1/credential/${encodeURIComponent(supportedParams.vcId)}`,
+				{
+					method: 'GET',
+					headers,
+					redirect: 'follow',
+				},
+			)
+			const result = JSON.parse(await response.text())
+
+			// 5. Return a boolean indicating the credential is valid
+			res.status(200).json({ data: result.vc })
+		}
+	} catch (error: any) {
+		res.status(500).json({ error: error.message })
+	}
+})
+
+export const verifyDiscoCredential = functions.https.onRequest(async (req, res) => {
+	try {
+		// 1. Validate required inputs
+		const supportedParams = {
+			email: req.body.email,
+			recipientDID: req.body.recipientDID,
+			vcId: req.body.vcId,
+		}
+		const validateDIDResult = await validateAndConvert(DiscoRecipientDto, {
+			did: supportedParams.recipientDID,
+		})
+		const validateEmailResult = await validateAndConvert(UserEmailDto, {
+			email: supportedParams.email,
+		})
+
+		// 2. Catch validation errors if any
+		if (validateDIDResult.error) {
+			res.status(400).json({ error: validateDIDResult.error })
+		} else if (validateEmailResult.error) {
+			res.status(400).json({ error: validateEmailResult.error })
+		} else {
+			// 3a. Look up the user by email, fail if doesn't exist
+			const userSnapshot = await admin
+				.firestore()
+				.collection(COLLECTIONS.USERS)
+				.where('email', '==', supportedParams.email)
+				.get()
+			if (userSnapshot.empty)
+				throw new Error(`User with email '${supportedParams.email}' could not be found`)
+			const userRef = userSnapshot.docs[0].ref
+
+			// 3b. Look up the credential in the db, fail if doesn't exist
+			const userVcSnapshot = await userRef
+				.collection(SUBCOLLECTIONS.DISCO_CREDENTIALS)
+				.where('vc.id', '==', supportedParams.vcId)
+				.get()
+			if (userVcSnapshot.empty)
+				throw new Error(`VC with id ${supportedParams.vcId} could not be found`)
+			// Get the 'vc' field
+			const vcRef = userVcSnapshot.docs[0].ref
+			const vcDoc = await vcRef.get()
+			const { vc: vcData } = vcDoc.data()
+			console.log({ vcData })
+
+			// 4. Make a request to Disco API to verify the credential is valid
+			const headers = new Headers()
+			headers.append('Content-Type', 'application/json')
+			headers.append('Accept', '*/*')
+			headers.append('Authorization', `Bearer ${process.env.DISCO_ORG_API_KEY}`)
+			const response = await fetch(`https://api.disco.xyz/v1/credential/verify`, {
+				method: 'POST',
+				headers,
+				body: JSON.stringify(vcData),
+				redirect: 'follow',
+			})
+			const result = await response.text()
+
+			// 5. Return the result, which will be a boolean indicating if credential is valid or not
+			res.status(200).json({ data: result })
 		}
 	} catch (error: any) {
 		console.error(error)
