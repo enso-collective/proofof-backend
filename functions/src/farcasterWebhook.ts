fda5b6e0ef94577704bb9168080c2409f379a8bb
Sean Brennan <sean@relay.cc>
2024-02-29 18:35:51 -0700

    update prompt for brand detection

--- functions/src/farcasterWebhook.ts
+++ functions/src/farcasterWebhook.ts
@@ -61,7 +61,7 @@ export const farcasterWebhook = functions.https.onRequest(async (req, res) => {
         }, 
         {
             role: 'user',
-            content: `A user supplied the following description which is meant to contain a brand name. Your job is to choose the primary brand they are describing. If no brands are mentioned, then return an empty response. If they say "[Brand] at the SheFi Summit" then choose the [Brand].
+            content: `A user supplied the following description which is meant to contain a brand name. Your job is to choose the primary brand they are describing. If no brands are mentioned, then return an empty array []. If they say "[Brand] at a [Place]" then choose the [Brand].
             User description: ${data.message}`
         }],
         model: 'gpt-4-0125-preview',
