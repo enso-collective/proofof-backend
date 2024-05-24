e4d8051c04f8383d8661c22526100475bfb7fce6
Sean Brennan <sean@relay.cc>
2024-05-24 12:49:31 +0200

    update tweet logic to process #ProofofLUKSO hashtag

--- functions/src/twitterScheduler.ts
+++ functions/src/twitterScheduler.ts
@@ -48,123 +48,105 @@ export const twitterScheduler = onSchedule('* * * * *', async (event) => {
         const appClient = new Client(TWITTER_API_BEARER_KEY!);
         const userClient = new Client(authClient);
 
-        const mentions = await appClient.tweets.usersIdMentions('1758104296208396288', { start_time: '2024-01-27T07:05:14.227Z', since_id: lastMentionTweetId, expansions: ['author_id', 'entities.mentions.username', 'attachments.media_keys', 'in_reply_to_user_id', 'referenced_tweets.id'], 'media.fields': ['url', 'type', 'variants', 'preview_image_url'], 'user.fields': ['username', 'id', 'name'], 'tweet.fields': ['attachments', 'author_id', 'text', 'id', 'in_reply_to_user_id'] });
-        let newestId = mentions.meta?.newest_id;
+        type MentionData = {
+            id: string;
+            text: string;
+            author_id: string;
+            referenced_tweets?: { id: string; type: "retweeted" | "quoted" | "replied_to" }[];
+            attachments?: { media_keys: string[] };
+            edit_history_tweet_ids: string[];
+        };
 
-        const hashtagMentions = await appClient.tweets.tweetsRecentSearch({ query: '#ProofofLUKSO', since_id: lastLukosMentionId, expansions: ['author_id', 'entities.mentions.username', 'attachments.media_keys', 'in_reply_to_user_id', 'referenced_tweets.id'], 'media.fields': ['url', 'type', 'variants', 'preview_image_url'], 'user.fields': ['username', 'id', 'name'], 'tweet.fields': ['attachments', 'author_id', 'text', 'id', 'in_reply_to_user_id'] });
-        const newestMentionId = hashtagMentions.meta?.newest_id;
+        const mentions = await appClient.tweets.usersIdMentions('1758104296208396288', {
+            start_time: '2024-01-27T07:05:14.227Z',
+            since_id: lastMentionTweetId,
+            expansions: ['author_id', 'entities.mentions.username', 'attachments.media_keys', 'in_reply_to_user_id', 'referenced_tweets.id'],
+            'media.fields': ['url', 'type', 'variants', 'preview_image_url'],
+            'user.fields': ['username', 'id', 'name'],
+            'tweet.fields': ['attachments', 'author_id', 'text', 'id', 'in_reply_to_user_id']
+        });
 
-        if (newestId !== lastMentionTweetId) {
-            if (newestId !== undefined) {
-                await twitterSettingsCollection.doc('settings').set({ lastMentionTweetid: newestId }, { merge: true });
+        const hashtagMentions = await appClient.tweets.tweetsRecentSearch({
+            query: '#ProofofLUKSO',
+            since_id: lastLukosMentionId,
+            expansions: ['author_id', 'entities.mentions.username', 'attachments.media_keys', 'in_reply_to_user_id', 'referenced_tweets.id'],
+            'media.fields': ['url', 'type', 'variants', 'preview_image_url'],
+            'user.fields': ['username', 'id', 'name'],
+            'tweet.fields': ['attachments', 'author_id', 'text', 'id', 'in_reply_to_user_id']
+        });
+
+        const processMentions = async (mentionsData: MentionData[], newestId: string | undefined, lastMentionId: string | undefined, mentionType: string) => {
+            if (newestId !== lastMentionId) {
+                if (newestId !== undefined) {
+                    await twitterSettingsCollection.doc('settings').set({ [mentionType]: newestId }, { merge: true });
+                }
+
+                const tweetsCollection = admin.firestore().collection('PossibleMention');
+                const userCollection = admin.firestore().collection('User');
+
+                const media = mentions.includes?.media ?? [];
+                const users = mentions.includes?.users ?? [];
+
+                mentionsData?.forEach(async element => {
+                    if (element.referenced_tweets != null) { return; }
+                    
+                    const user = users.find(x => x.id === element.author_id);
+                    const username = user?.username;
+
+                    if (!username) {
+                        console.log('Username is undefined, skipping this mention.');
+                        return;
+                    }
+
+                    console.log(`username: ${username}`);
+                    const usersSnapshot = await userCollection.where('twitterUsername', '==', username).get();
+
+                    await tweetsCollection.add({ username: username, data: element });
+
+                    if (usersSnapshot.empty) {
+                        return;
+                    }
+
+                    const wallet = usersSnapshot.docs![0].data().userWalletLower;
+                    const brandName = await extractBrand(element.text);
+
+                    if (brandName === null) {
+                        await userClient.tweets.createTweet({ text: `We didn't find a clear brand or quest described in your tweet @${username}. Please retry your tweet with more specific description of the brand or the quest hashtag.`, reply: { in_reply_to_tweet_id: element.id } });
+                        return;
+                    }
+
+                    const mediaKeys = element.attachments?.media_keys ?? [];
+                    const photo = media.find(x => mediaKeys.includes(x.media_key ?? '') && x.type === 'photo') as components['schemas']['Photo'];
+
+                    if (photo === undefined && brandName !== 'LUKSO') {
+                        await userClient.tweets.createTweet({ text: `I didn't see an image attached to your tweet @${username}, please retry a new tweet with an image.`, reply: { in_reply_to_tweet_id: element.id } });
+                        return;
+                    }
+                    
+                    const brandValidation = await validateBrand(brandName, element.text, photo.url!);
+                    if (brandValidation === null) {
+                        await userClient.tweets.createTweet({ text: `@${username} the AI analysis of your description & image determined it to be "${brandValidation}" for a Proof. Please try again with a different image or description.`, reply: { in_reply_to_tweet_id: element.id } });
+                        return;
+                    }
+
+                    let questId = determineQuestId(brandName);
+
+                    const tweetUrl = `https://twitter.com/${username}/status/${newestId}`;
+                    const hash = await eas_mint(username, wallet, tweetUrl, photo.url!, element.text, questId);
+                    await userClient.tweets.createTweet({ text: `@${username} your ${brandName} Proof is created! Create your Universal Profile, connect your Twitter, and view your attestations and leaderboard on https://www.proofof.bot/events/lukso`, reply: { in_reply_to_tweet_id: element.id } }); 
+                });
             }
-    
-            const userCollection = admin.firestore().collection('User');
-    
-            const media = mentions.includes?.media ?? []
-            const users = mentions.includes?.users ?? []
-    
-            mentions.data?.forEach(async element => {
-                if (element.referenced_tweets != null) { return; }
-                
-                const user = users.find(x => x.id === element.author_id);
-                console.log(`username: ${user?.username}`);
-                const usersSnapshot = await userCollection.where('twitterUsername', '==', user?.username).get()
-    
-                if (usersSnapshot.empty) {
-                    await userClient.tweets.createTweet({ text: `A connected wallet is required for your onchain Proof, please sign up on https://proofof.bot and connect your Twitter`, reply: { in_reply_to_tweet_id: element.id } });
-                    return;
-                }
-    
-                const wallet = usersSnapshot.docs![0].data().userWalletLower;
-    
-    
-                const brandName = await extractBrand(element.text);
-    
-                if (brandName === null) {
-                    await userClient.tweets.createTweet({ text: `We didn't find a clear brand or quest described in your tweet @${user?.username}. Please retry your tweet with more specific description of the brand or the quest hashtag.`, reply: { in_reply_to_tweet_id: element.id } });
-    
-                    return;
-                }
-    
-                const mediaKeys = element.attachments?.media_keys ?? [];
-                const photo = media.find(x => mediaKeys.includes(x.media_key ?? '') && x.type === 'photo') as components['schemas']['Photo'];
-    
-                if (photo === undefined && brandName !== 'LUKSO') {
-                    await userClient.tweets.createTweet({ text: `I didn't see an image attached to your tweet @${user?.username}, please retry a new tweet with an image.`, reply: { in_reply_to_tweet_id: element.id } });
-                    return;
-                }
-                
-                const brandValidation = await validateBrand(brandName, element.text, photo.url!);
-                if (brandValidation === null) {
-                    await userClient.tweets.createTweet({ text: `@${user?.username} the AI analysis of your description & image determined it to be "${brandValidation}" for a Proof. Please try again with a different image or description.`, reply: { in_reply_to_tweet_id: element.id } });
-                    return;
-                }
-    
-                let questId = determineQuestId(brandName);
-    
-                const tweetUrl = `https://twitter.com/${user?.username}/status/${newestId}`;
-                const hash = await eas_mint(user?.username!, wallet, tweetUrl, photo.url!, element.text, questId);
-                await userClient.tweets.createTweet({ text: `@${user?.username} your ${brandName} Proof is created! Create your Universal Profile, connect your Twitter, and view your attestation and leaderboard on https://www.proofof.bot/events/lukso`, reply: { in_reply_to_tweet_id: element.id } }); 
+        };
 
-            });
-        }
+        const newestMentionId = mentions.meta?.newest_id;
+        const newestHashtagMentionId = hashtagMentions.meta?.newest_id;
+        
+        const validMentions = (mentions.data ?? []).filter((mention): mention is MentionData => mention.author_id !== undefined);
+        const validHashtagMentions = (hashtagMentions.data ?? []).filter((mention): mention is MentionData => mention.author_id !== undefined);
 
-        if (newestMentionId !== lastLukosMentionId) {
-            if (newestMentionId !== undefined) {
-                await twitterSettingsCollection.doc('settings').set({ lastLukosMentionId: newestMentionId }, { merge: true });
-            }
-
-            const tweetsCollection = admin.firestore().collection('PossibleMention');
-            const userCollection = admin.firestore().collection('User');
-    
-            const media = mentions.includes?.media ?? []
-            const users = mentions.includes?.users ?? []
-
-            hashtagMentions.data?.forEach(async element => {
-                if (element.referenced_tweets != null) { return; }
-                
-                const user = users.find(x => x.id === element.author_id);
-                console.log(`username: ${user?.username}`);
-                const usersSnapshot = await userCollection.where('twitterUsername', '==', user?.username).get()
-
-                await tweetsCollection.add({ username: user?.username, data: element });
-
-                if (usersSnapshot.empty) {
-                    return;
-                }
-
-                const wallet = usersSnapshot.docs![0].data().userWalletLower;
-                const brandName = await extractBrand(element.text);
-    
-                if (brandName === null) {
-                    await userClient.tweets.createTweet({ text: `We didn't find a clear brand or quest described in your tweet @${user?.username}. Please retry your tweet with more specific description of the brand or the quest hashtag.`, reply: { in_reply_to_tweet_id: element.id } });
-    
-                    return;
-                }
-    
-                const mediaKeys = element.attachments?.media_keys ?? [];
-                const photo = media.find(x => mediaKeys.includes(x.media_key ?? '') && x.type === 'photo') as components['schemas']['Photo'];
-    
-                if (photo === undefined && brandName !== 'LUKSO') {
-                    await userClient.tweets.createTweet({ text: `I didn't see an image attached to your tweet @${user?.username}, please retry a new tweet with an image.`, reply: { in_reply_to_tweet_id: element.id } });
-                    return;
-                }
-                
-                const brandValidation = await validateBrand(brandName, element.text, photo.url!);
-                if (brandValidation === null) {
-                    await userClient.tweets.createTweet({ text: `@${user?.username} the AI analysis of your description & image determined it to be "${brandValidation}" for a Proof. Please try again with a different image or description.`, reply: { in_reply_to_tweet_id: element.id } });
-                    return;
-                }
-    
-                let questId = determineQuestId(brandName);
-    
-                const tweetUrl = `https://twitter.com/${user?.username}/status/${newestMentionId}`;
-                const hash = await eas_mint(user?.username!, wallet, tweetUrl, photo.url!, element.text, questId);
-                await userClient.tweets.createTweet({ text: `@${user?.username} your ${brandName} Proof is created! Create your Universal Profile, connect your Twitter, and view your attestations and leaderboard on https://www.proofof.bot/events/lukso`, reply: { in_reply_to_tweet_id: element.id } }); 
-            });
-        }
-    } catch(error) {
-        console.log(error);
+        await processMentions(validMentions, newestMentionId, lastMentionTweetId, 'lastMentionTweetid');
+        await processMentions(validHashtagMentions, newestHashtagMentionId, lastLukosMentionId, 'lastLukosMentionId');
+    } catch (error) {
+        console.error('Error processing Twitter mentions:', error);
     }
 });
