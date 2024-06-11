28435d7664da2517b170ad416af16ed758ab85aa
Stanislav Ageev <stanislav@agitek.io>
2024-06-12 00:21:10 +0500

    Reply to user on lens

--- functions/src/lensWebhook.ts
+++ functions/src/lensWebhook.ts
@@ -6,9 +6,13 @@ import { lensWebhookInput } from './schemes';
 import { validateBrand, extractBrand } from './aiValidations'
 import { determineQuestId } from './ensoUtils'
 import { eas_mint } from './mint'
+import { textOnly } from '@lens-protocol/metadata'
+import PinataSDK from '@pinata/sdk'
 
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY as string;
 const PROFILE_ID = process.env.PROFILE_ID;
+
+let pinataClient = new PinataSDK({ pinataApiKey: '32f196b95bef76b93fc1', pinataSecretApiKey: '487b382ea6e3d464cdae8344aa849b9c82700f1670408245f9876013a664387a' });
 
 class FirebaseLensStorageProvider implements IStorageProvider {
   readonly collection = admin.firestore().collection('lensAuth');
@@ -57,11 +61,8 @@ export const lensWebhook = functions.https.onRequest(async (req, res) => {
     res.sendStatus(200);
 
     const profile = await lensClient.profile.fetch({ forProfileId: data.profileId })
-    const localHandle = profile?.handle?.localName
     const handle = profile?.handle?.fullHandle
     const ownedBy = profile?.handle?.ownedBy
-
-    console.log(`LocalHandle: ${localHandle}, FullHandle: ${handle}, Ownedby: ${ownedBy}`)
 
     const publication = await lensClient.publication.fetch({ forId: data.publicationId });
 
@@ -71,18 +72,16 @@ export const lensWebhook = functions.https.onRequest(async (req, res) => {
 
     const pub = publication as PostFragment;
     if (pub.metadata.__typename !== 'ImageMetadataV3') {
-      console.log(`I didn't see an image attached to your cast @${handle}, please retry a new cast with an image.`);
+      lensComment(data.publicationId, `I didn't see an image attached to your cast @${handle}, please retry a new cast with an image.`);
       return;
     }
     
     const meta = pub.metadata as ImageMetadataV3Fragment
-    console.log(meta.asset.image.optimized);
-    console.log(meta.content);
 
     const brandName = await extractBrand(meta.content);
 
     if (brandName === null) {
-      console.log(`We didn't find a clear brand or quest described in your cast @${handle}. Please retry your publication with more specific description of the brand or quest hashtag.`);
+      lensComment(data.publicationId, `We didn't find a clear brand or quest described in your cast @${handle}. Please retry your publication with more specific description of the brand or quest hashtag.`);
       console.log('Cannot extract brand name');
       return
     }
@@ -90,15 +89,15 @@ export const lensWebhook = functions.https.onRequest(async (req, res) => {
     const imageUrl = meta.asset.image.optimized?.uri!
     const brandValidation = await validateBrand(brandName, meta.content, meta.asset.image.optimized?.uri!);
     if (brandValidation === null) {
-        console.log(`@${handle} the AI analysis of your description & image determined it to be "${brandValidation}" for a Proof. Please try again with a different image or description.`);
-        return;
+      lensComment(data.publicationId, `@${handle} the AI analysis of your description & image determined it to be "${brandValidation}" for a Proof. Please try again with a different image or description.`);
+      return;
     }
 
     let questId = determineQuestId(brandName);
     const pubURL = `https://hey.xyz/posts/${data.publicationId}`;
     const hash = await eas_mint(handle!, ownedBy!, pubURL, imageUrl, meta.content, questId);
 
-    console.log(`@${handle} your ${brandName} Proof is minted! View the transaction on Base: https://www.onceupon.gg/${hash}`);
+    lensComment(data.publicationId, `@${handle} your ${brandName} Proof is minted! View the transaction on Base: https://www.onceupon.gg/${hash}`);
   }  catch(error) {
     console.log(error);
   }
@@ -120,3 +119,17 @@ export const lensAuth = functions.https.onRequest(async (req, res) => {
   await lensClient.authentication.authenticate({ id, signature });
   res.sendStatus(200);
 });
+
+const lensComment = async (on: string, text: string) => {
+  const textMetadata = textOnly({ content: text });
+  const response = await pinataClient.pinJSONToIPFS(textMetadata);
+  await lensCommentSend(on, `ipfs://${response.IpfsHash}`);
+}
+
+const lensCommentSend = async (on: string, uri: string) => {
+  try {
+    lensClient.publication.commentOnMomoka({ commentOn: on, contentURI: uri });
+  } catch {
+    setTimeout(async function() { await lensComment(on, uri); }, 10000);
+  }
+}
